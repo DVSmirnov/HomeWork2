2c1571310b28ad0c560d14fe9f6aae48286a747a
DVSmirnov <smirnovdd@inbox.ru>
2022-07-21 11:45:20 +0400

    Добавил фигуры и тесты

---
+++ src/Rectangle.py
@@ -0,0 +1,20 @@
+from src.Figure import Figure
+
+
+class Rectangle(Figure):
+    NAME = 'Rectangle'
+
+    def __init__(self, x, y):
+        if x <= 0 or y <= 0:
+            raise ValueError("Стороны прямоугольника должны быть больше нуля")
+        self.x = x
+        self.y = y
+
+    @property
+    def get_area(self):
+        return self.x * self.y
+
+    @property
+    def get_pr(self):
+        return 2 * (self.x + self.y)
+
