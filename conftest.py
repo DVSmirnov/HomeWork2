5ee096165dc347f93b7c984bbbb4e485d2c97455
DVSmirnov <smirnovdd@inbox.ru>
2022-07-24 15:01:42 +0400

    Adding files

---
+++ conftest.py
@@ -0,0 +1,26 @@
+import pytest
+
+from src.Triangle import Triangle
+from src.Circle import Circle
+from src.Rectangle import Rectangle
+from src.Square import Square
+
+
+@pytest.fixture
+def rectangle():
+    return Rectangle(5, 10)
+
+
+@pytest.fixture
+def triangle():
+    return Triangle(5, 10, 12)
+
+
+@pytest.fixture
+def square():
+    return Square(7)
+
+
+@pytest.fixture
+def circle():
+    return Circle(25)
